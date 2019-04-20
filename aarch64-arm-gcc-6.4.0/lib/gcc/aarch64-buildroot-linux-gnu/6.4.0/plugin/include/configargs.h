a27ffb7ab683305302287b7627d7257ecc187b87
novasomindustries <novasomindustries@Deb9.Deb9>
2019-04-20 16:18:09 +0200

    Initial import

---
+++ aarch64-arm-gcc-6.4.0/lib/gcc/aarch64-buildroot-linux-gnu/6.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/Devel/NOVAsdk2019.07/Xcompiler/aarch64-arm-gcc-6.4.0 --sysconfdir=/Devel/NOVAsdk2019.07/Xcompiler/aarch64-arm-gcc-6.4.0/etc --enable-static --target=aarch64-buildroot-linux-gnu --with-sysroot=/Devel/NOVAsdk2019.07/Xcompiler/aarch64-arm-gcc-6.4.0/aarch64-buildroot-linux-gnu/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/Devel/NOVAsdk2019.07/Xcompiler/aarch64-arm-gcc-6.4.0 --with-mpc=/Devel/NOVAsdk2019.07/Xcompiler/aarch64-arm-gcc-6.4.0 --with-mpfr=/Devel/NOVAsdk2019.07/Xcompiler/aarch64-arm-gcc-6.4.0 --with-pkgversion='Buildroot 2018.02.12' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --enable-plugins --enable-lto --disable-libmudflap --enable-threads --with-isl=/Devel/NOVAsdk2019.07/Xcompiler/aarch64-arm-gcc-6.4.0 --disable-decimal-float --with-abi=lp64 --with-cpu=cortex-a53 --enable-languages=c,c++ --with-build-time-tools=/Devel/NOVAsdk2019.07/Xcompiler/aarch64-arm-gcc-6.4.0/aarch64-buildroot-linux-gnu/bin --enable-shared --enable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "cpu", "cortex-a53" } };
---
+++ arch32-arm-gcc-5.5.0/lib/gcc/arm-buildroot-linux-gnueabihf/5.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/Devel/NOVAsdk/Xcompiler/arch32-arm-gcc-5.5.0 --sysconfdir=/Devel/NOVAsdk/Xcompiler/arch32-arm-gcc-5.5.0/etc --enable-static --target=arm-buildroot-linux-gnueabihf --with-sysroot=/Devel/NOVAsdk/Xcompiler/arch32-arm-gcc-5.5.0/arm-buildroot-linux-gnueabihf/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/Devel/NOVAsdk/Xcompiler/arch32-arm-gcc-5.5.0 --with-mpc=/Devel/NOVAsdk/Xcompiler/arch32-arm-gcc-5.5.0 --with-mpfr=/Devel/NOVAsdk/Xcompiler/arch32-arm-gcc-5.5.0 --with-pkgversion='Buildroot 2018.02.12' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --enable-plugins --enable-lto --disable-libmudflap --enable-threads --with-isl=/Devel/NOVAsdk/Xcompiler/arch32-arm-gcc-5.5.0 --disable-decimal-float --with-abi=aapcs-linux --with-cpu=cortex-a9 --with-fpu=vfpv3-d16 --with-float=hard --with-mode=arm --enable-languages=c,c++ --with-build-time-tools=/Devel/NOVAsdk/Xcompiler/arch32-arm-gcc-5.5.0/arm-buildroot-linux-gnueabihf/bin --enable-shared --enable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a9" }, { "float", "hard" }, { "mode", "arm" }, { "fpu", "vfpv3-d16" }, { "tls", "gnu" } };
